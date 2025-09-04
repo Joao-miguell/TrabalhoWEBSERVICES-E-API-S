0af12ea2625a983fb9b7fb6b0e0f9deffa654589
João Miguel <joaocarrard2006@gmail.com>
2025-09-03 23:15:38 -0300

    .

---
+++ src/Application/UseCases/Auth/RegisterUser.js
@@ -0,0 +1,25 @@
+// src/Application/UseCases/Auth/RegisterUser.js
+const User = require('src/Domain/User/User');
+const UserOutput = require('src/Application/DTOs/UserOutput');
+const UserAlreadyExistsException = require('src/Domain/Exceptions/UserAlreadyExistsException');
+
+class RegisterUser {
+  constructor(userRepository) {
+    this.userRepository = userRepository; // IUserRepository
+  }
+
+  async execute(input) { // input é RegisterUserInput
+    const existingUser = await this.userRepository.findByEmail(input.email);
+    if (existingUser) {
+      throw new UserAlreadyExistsException('User with this email already exists.');
+    }
+
+    const user = new User(input.name, input.email, input.password);
+
+    await this.userRepository.save(user);
+
+    return new UserOutput(user);
+  }
+}
+
+module.exports = RegisterUser;
